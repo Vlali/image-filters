caab974d0ba796889e12c94b5b06be882060e8b0
ludovitvendrics <lali.vendrics@outlook.com>
2025-04-28 06:14:51 +0200

    .

--- src/main/java/filters/Subsampling.java
+++ src/main/java/filters/Subsampling.java
@@ -21,26 +21,46 @@ public class Subsampling implements Filter {
 		BufferedImage bi = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
 	
 		/* @TODO Place your implementation here */
+
+		// get the different blocks
 		for(int x = 0; x < image.getWidth(); x+=rate) {
 			for(int y = 0; y < image.getHeight(); y+=rate) {
 
-				Pixel pixel = new Pixel(image.getRGB(x, y));
-				pixel.getCb();
-				pixel.getCr();
+				Pixel pixelWithinTheBlock = new Pixel(image.getRGB(x, y));
+
+				int pixelWithinTheBlocksCb=pixelWithinTheBlock.getCb();
+				int pixelWithinTheBlocksCr =pixelWithinTheBlock.getCr();
 
 
+				// inside every single block
+				for(int x1 = 0; x1 < rate; x1++) {
+					for(int y1 = 0; y1 < rate; y1++) {
 
+						int pixelBlockCoordinateX=x+x1;
+						int pixelBlockCoordinateY=y+y1;
 
+						if(pixelBlockCoordinateX< image.getWidth() && pixelBlockCoordinateY < image.getHeight()) {
+							Pixel currentPixel = new Pixel(image.getRGB(pixelBlockCoordinateX, pixelBlockCoordinateY));
 
-				int newRaw= Pixel.generateRaw(gray,gray,gray,pixel.getAlpha());
+							currentPixel.setCb(pixelWithinTheBlocksCb);
+							currentPixel.setCr(pixelWithinTheBlocksCr);
 
-				bi.setRGB(x, y, newRaw);
+							int red=currentPixel.getR();
+							int green=currentPixel.getG();
+							int blue=currentPixel.getB();
+							int alpha=currentPixel.getAlpha();
+
+							int newRaw=Pixel.generateRaw(red, green, blue, alpha);
+
+							bi.setRGB(pixelBlockCoordinateX, pixelBlockCoordinateY, newRaw);
+
+						}
+					}
+				}
 
 			}
 		}
 
-
-		
 		return bi;
 	}
 
